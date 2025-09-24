58f33e38caa2c268d3cdc29db793d3c7e015a06d
hannahsundar16 <_hannahsundar_@Hannahs-MacBook-Air-4.local>
2025-09-24 13:54:49 -0400

    Fix type error by updating IndBox props and adding use client directive

--- app/projPage/page.tsx
+++ app/projPage/page.tsx
@@ -1,4 +1,5 @@
-// ./app/projPage/page.tsx
+"use client";
+
 import Navbar from '../components/header';
 import { IndBox } from '../components/IndependentCard';
 
@@ -15,7 +16,7 @@ const projPage = () => {
               Recent Projects
             </h1>
           </div>
-        </div>    
+        </div>
 
         <div className="max-w-7xl w-full grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-6">
 
